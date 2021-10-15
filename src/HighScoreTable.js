3ddbbe25999f037042227d8c4f07c676fc6f041a
maham511 <maha.malik511@gmail.com>
2021-10-15 11:05:32 +0100

    Refactored

--- src/HighScoreTable.js
+++ src/HighScoreTable.js
@@ -15,15 +15,14 @@ import allCountryScores from "./scores";
 console.log(allCountryScores); //logs arr of 8 obj elems
 
 const AllTables = () => {
-return (
-    <div className='wrapper'>
+  return (
+    <div className="wrapper">
       <h2>High Scores per Country</h2>
-      {allCountryScores.map((countryInfo) => (
-      <HighScoreTable countryInfo={countryInfo}/>
-    )   
-    )}
+      {allCountryScores.map((country) => (
+        <HighScoreTable country={country} />
+      ))}
     </div>
-);
+  );
 };
 
 //map through scores arr
@@ -31,18 +30,15 @@ return (
 //pass name & score as 2 different props in PlayerScore component
 //Use props in playerscore to rnder row
 
-const HighScoreTable = ({ countryInfo }) => {
-  
-    console.log(countryInfo.scores);
-  
+const HighScoreTable = ({ country }) => {
   return (
     <div className="country-div">
-      <h3>High Scores: {countryInfo.name}</h3>
+      <h3>High Scores: {country.name}</h3>
       <table>
         <tbody>
-          {/*Maps through scores arr, renders playerscore row for each player  */}
-          {countryInfo.scores.map((playerScore) => (
-            <PlayerScore name={playerScore.n} score={playerScore.s} />
+          {/*Map scores arr, render PlayerScore row for each player. Name & score props created to access n & s values  */}
+          {country.scores.map((score) => (
+            <PlayerScore name={score.n} score={score.s} />
           ))}
         </tbody>
       </table>
@@ -50,6 +46,7 @@ const HighScoreTable = ({ countryInfo }) => {
   );
 };
 
+//name & score props passed, both values will render in own td (1 tr)
 const PlayerScore = ({ name, score }) => {
   return (
     <tr>
@@ -59,28 +56,5 @@ const PlayerScore = ({ name, score }) => {
     </tr>
   );
 };
-
-// const HighScoreTable = (props) => {
-//   return (
-//     <div className="country-div">
-//       <h3>High Scores: {props.countryInfo.name}</h3>
-//       <table>
-//         <tbody>
-//           <PlayerScore name={} score={}/>
-//         </tbody>
-//       </table>
-//     </div>
-//   );
-// };
-
-// const PlayerScore = (props) => {
-//   return (
-//     <tr>
-//       <th scope="row"></th>
-//       <td>{props.}</td>
-//       <td>{props.}</td>
-//     </tr>
-//   );
-// };
 
 export default AllTables;
