1e8e5617aa76843d513f1cbf04a5e88ca7859397
maham511 <maha.malik511@gmail.com>
2021-10-15 20:22:28 +0100

    Css added to tables

--- src/App.js
+++ src/App.js
@@ -8,9 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <div class="wrapper">
           <AllTables/>
-        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
